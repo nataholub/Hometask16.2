2ebb66eb8b97ea0b18481e03d7a3acfd9b283729
Natalia Holub <nata.holub@ukr.net>
2025-10-18 23:34:42 +0300

    Hometask

---
+++ Hometask16.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+try
+{
+    int inputNumber;
+    
+    Console.Write("Enter the number: ");
+    try
+    {
+        inputNumber = int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid input.Please enter numeric values only.");
+        return;
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number is too large or too small for an int type.");
+        return;
+    }
+    catch (ArgumentNullException)
+    {
+        Console.WriteLine("Input value is missing.");
+        return;
+    }
+    catch (IOException)
+    {
+        Console.WriteLine("Console error.");
+        return;
+    }
+    catch (OutOfMemoryException)
+    {
+        Console.WriteLine("Insufficient memory.");
+        return;
+    }
+
+    Console.WriteLine("The entered value is a number.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Сталася помилка: {ex.Message}");
+}
